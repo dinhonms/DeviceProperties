b3d58e29898af76db62a6585564de17e341c8ca0
dinhonms <dinhonms@gmail.com>
2023-06-27 08:41:43 -0300

    Initial Commit

---
+++ DeviceProperties/DeviceProperties.h
@@ -0,0 +1,18 @@
+//
+//  DeviceProperties.h
+//  DeviceProperties
+//
+//  Created by Nonato Sousa on 27/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceProperties.
+FOUNDATION_EXPORT double DevicePropertiesVersionNumber;
+
+//! Project version string for DeviceProperties.
+FOUNDATION_EXPORT const unsigned char DevicePropertiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceProperties/PublicHeader.h>
+
+
